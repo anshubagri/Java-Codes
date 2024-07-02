c2892ae21a02f7cde51a55d87261752792009346
Anshu Singh Bagri <anshu.bagri94@gmail.com>
2024-07-02 20:29:20 +0530

    Create implicitconversion.java

---
+++ implicitconversion.java
@@ -0,0 +1,7 @@
+public class implicitconversion {
+    public static void main(String[] args) {
+        int count = 12;
+        long newCount = count;
+        System.out.println(newCount);
+    }
+}
