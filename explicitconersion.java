b710352d6ff42fbcd4ab6fea0d75a14d35edadcb
Anshu Singh Bagri <anshu.bagri94@gmail.com>
2024-07-02 20:29:16 +0530

    Create explicitconersion.java

---
+++ explicitconersion.java
@@ -0,0 +1,7 @@
+public class explicitconersion {
+    public static void main(String[] args) {
+        int age = 150;
+        byte newAge = (byte)age;
+        System.out.println(newAge);
+    }
+}
