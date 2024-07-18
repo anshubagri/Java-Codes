4c97333d327b7ec12bbe3d0647a0bba5debf29eb
Anshu Singh Bagri <anshu.bagri94@gmail.com>
2024-07-18 09:47:28 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,17 @@
+public class arrays {
+    public static void main(String[] args) {
+        int num[] = new int[5];
+        num[0] = 12;
+        num[1] = 23;
+        num[3] = 9;
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+        System.out.println(num[4]);
+        System.out.println(num.length);
+
+        int marks[] = {90 , 45 , 67 , 56 , 99};
+        System.out.println(marks[1]);
+    }
+}
